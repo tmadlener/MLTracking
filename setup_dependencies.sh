972d63a9f84e06d842b8e5517ade5d8bb09090bd
Thomas Madlener <thomas.madlener@desy.de>
2025-09-30 15:42:21 +0200

    Make setup dependencies script more robust

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Check if k4_local_repo function is defined, if not source utilities.sh
 if ! declare -f k4_local_repo > /dev/null; then
-    script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     . "$script_dir/utilities.sh"
 fi
 
-export CMAKE_PREFIX_PATH=$(pwd)/pytorch_scatter/install:$CMAKE_PREFIX_PATH
-cd acts && k4_local_repo && cd ..
-cd kActsTracking && k4_local_repo && cd ..
+curr_dir=$(pwd)
+
+export CMAKE_PREFIX_PATH=$(relpath ${script_dir}/../pytorch_scatter/install):$CMAKE_PREFIX_PATH
+cd ${script_dir}/../acts && k4_local_repo
+cd ${script_dir}/../kActsTracking && k4_local_repo
